a79478cc94afcb143a58bf532dca6ab80184427c
millermvs <millermvs@gmail.com>
2026-01-19 17:44:58 -0300

    feat: Adiciona anotação @Configuration na classe WebClientConfiguration

--- src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/infrastructure/configurations/WebClientConfiguration.java
+++ src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/infrastructure/configurations/WebClientConfiguration.java
@@ -1,8 +1,10 @@
 package br.com.automica.api.whatsapp.modules.whatsapp.infrastructure.configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfiguration {
 
     @Bean
