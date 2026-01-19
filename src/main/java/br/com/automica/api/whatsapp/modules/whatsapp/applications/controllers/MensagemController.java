9664c5b28a79d367e558da8784fbc35637b949d1
millermvs <millermvs@gmail.com>
2026-01-19 20:38:43 -0300

    feat: Adiciona métodos para envio de mensagens e verificação de webhook na classe MensagemController e WhatsAppWebhookController

--- src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/applications/controllers/MensagemController.java
+++ src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/applications/controllers/MensagemController.java
@@ -1,10 +1,34 @@
 package br.com.automica.api.whatsapp.modules.whatsapp.applications.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.automica.api.whatsapp.modules.whatsapp.domain.dtos.request.mensagem.MensagemRequestDto;
+import br.com.automica.api.whatsapp.modules.whatsapp.domain.services.MensagemService;
+
+
 
 @RestController
 @RequestMapping("/whatsapp/mensagens")
 public class MensagemController {
 
+	@Autowired
+	MensagemService mensagemService;
+
+	@PostMapping("enviar")
+	public ResponseEntity<String> post(@RequestBody MensagemRequestDto request) {
+		var response = mensagemService.enviarMensagem(request);
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("template")
+	public ResponseEntity<String> postTemplate(@RequestParam String destinatario) {
+		var response = mensagemService.enviarTemplate(destinatario);
+		return ResponseEntity.ok(response);
+	}
 }
--- src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/applications/controllers/WhatsAppWebhookController.java
+++ src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/applications/controllers/WhatsAppWebhookController.java
@@ -1,5 +1,54 @@
 package br.com.automica.api.whatsapp.modules.whatsapp.applications.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import br.com.automica.api.whatsapp.modules.whatsapp.domain.services.CaixaEntradaWebhookMetaService;
+import tools.jackson.databind.JsonNode;
+
+@RestController
+@RequestMapping("/webhook")
 public class WhatsAppWebhookController {
 
+	@Value("${meta.webhook.verify-token}")
+	private String expectedVerifyToken;
+	
+	@Autowired
+	private CaixaEntradaWebhookMetaService caixaEntradaWebhookMeta;
+
+	@GetMapping
+	public ResponseEntity<String> verificarWebhook(@RequestParam(name = "hub.mode", required = false) String mode,
+			@RequestParam(name = "hub.verify_token", required = false) String verifyToken,
+			@RequestParam(name = "hub.challenge", required = false) String challenge) {
+
+		boolean isSubscribe = "subscribe".equals(mode);
+		boolean tokenOk = expectedVerifyToken != null && expectedVerifyToken.equals(verifyToken);
+
+		if (isSubscribe && tokenOk) {
+			return ResponseEntity.ok(challenge);
+		}
+
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> receberWebhook(@RequestBody JsonNode payload) {
+		
+		caixaEntradaWebhookMeta.savePayload(payload);
+
+		System.out.println("WEBHOOK RECEBIDO: " + payload.toString());
+
+		return ResponseEntity.ok().build();
+	}
 }
+
