73992da20e98d404902a7b73aac17858339dd0df
millermvs <millermvs@gmail.com>
2026-01-19 21:27:55 -0300

    fix: Corrige a anotação @Value para a variável token na classe MetaWhatsAppClient

--- src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/infrastructure/meta/client/MetaWhatsAppClient.java
+++ src/main/java/br/com/automica/api/whatsapp/modules/whatsapp/infrastructure/meta/client/MetaWhatsAppClient.java
@@ -1,5 +1,6 @@
 package br.com.automica.api.whatsapp.modules.whatsapp.infrastructure.meta.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -11,7 +12,7 @@ public class MetaWhatsAppClient {
 
 	private final WebClient webClient;
 	
-	//@Value("${meta.token}")
+	@Value("${meta.token}")
 	private String token;
 
 	public MetaWhatsAppClient(WebClient webCliente) {
