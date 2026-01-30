10d1fa848ad0841417e017a7046dc2ba61a0d8b3
millermvs <millermvs@gmail.com>
2026-01-30 17:25:44 -0300

    feat: Adiciona listagem de conversas com paginação e tratamento de exceções para conversas não encontradas

--- src/main/java/br/com/automica/api/whatsapp/modules/conversa/applications/controllers/ConversasController.java
+++ src/main/java/br/com/automica/api/whatsapp/modules/conversa/applications/controllers/ConversasController.java
@@ -1,12 +1,15 @@
 package br.com.automica.api.whatsapp.modules.conversa.applications.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.automica.api.whatsapp.modules.conversa.domain.dtos.response.ListaDeConversasResponseDto;
 import br.com.automica.api.whatsapp.modules.conversa.domain.service.ConversaService;
 
 @RestController
@@ -20,6 +23,15 @@ public class ConversasController {
     public ResponseEntity<String> postCriarConversa(@RequestParam String waid, @RequestParam String phoneNumberId,
             @RequestParam String wabaId) {
         String response = conversaService.criarConversaSistema(waid, phoneNumberId, wabaId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("listar")
+    public ResponseEntity<Page<ListaDeConversasResponseDto>> getConversas(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size) {
+
+        var response = conversaService.listarConversas(page, size);
         return ResponseEntity.ok(response);
     }
 }
